8996597e2fe3bcc0b1276aee3960a698d2a6376a
vinoo selvarajah <selv@rajah.dev>
2024-10-06 18:28:20 +0200

    final

--- server.R
+++ server.R
@@ -5,44 +5,41 @@ library(deSolve)
 
 function(input, output) {
 
-  k1_values1 <- reactive({
-    c(
-      pro1Name = input$pro1Str,
-      pro2Name = input$pro2Str,
-      pro3Name = input$pro3Str,
-      pro4Name = input$pro4Str,
-      pro5Name = input$pro5Str
-    )
-  })
-  
-  
-  # Collect the k1 values into a list
-  promoter_strengths <- reactive({list(
-    input$pro1Str,
-    input$pro2Str,
-    input$pro3Str,
-    input$pro4Str,
-    input$pro5Str
-  )})
-  
-  # Collect the names of the promoters
-  promoter_names <- reactive({c(
-    input$pro1Name,
-    input$pro2Name,
-    input$pro3Name,
-    input$pro4Name,
-    input$pro5Name
-  )})
-  
-  k1_values <- reactive(setNames(promoter_strengths(), promoter_names()))
-  
-  
-  # reactive expression for solving the ODE and creating the data frame
-  all_results <- reactive({
-    all_results_df <- data.frame()
+  observeEvent( input$submitData, {
     
-    # parameters and states taken from https://youtu.be/K0P1KVk_hDo?si=5K72owA1k8SNi9u7&t=1046
-    # these will need to be checked
+    # NOTE: need to become dynamic, with user selectable # of inputs
+    # collect the promoter strengths
+    promoter_strengths <- c(
+      input$pro1Str,
+      input$pro2Str,
+      input$pro3Str,
+      input$pro4Str,
+      input$pro5Str
+    )
+    
+    # collect the names of the promoters
+    promoter_names <- c(
+      input$pro1Name,
+      input$pro2Name,
+      input$pro3Name,
+      input$pro4Name,
+      input$pro5Name
+    )
+    
+    # NOTE: needs better error handling, simple for now
+    # grab indices where promoter name is not empty, and strength exists and is greater than 0
+    valid_indices <- which(promoter_names != "" & !is.na(promoter_strengths) & promoter_strengths > 0)
+    
+    # only keep valid promoter names and strengths
+    valid_promoter_strengths <- promoter_strengths[valid_indices]
+    valid_promoter_names <- promoter_names[valid_indices]
+    
+    # create promoter name and strength as key-value pairs
+    # NOTE: probably an easier way to do this, coupled with error handling and above code
+    k1_values <- setNames(valid_promoter_strengths, valid_promoter_names)
+    
+    # all default parameters and states taken from https://youtu.be/K0P1KVk_hDo?si=5K72owA1k8SNi9u7&t=1046
+    # NOTE: these will need to be checked
     parameters <- c(
       k1 = NA, # initialize, but values will be added from k1_values in the loop
       d1 = input$mrnaDeg,
@@ -52,10 +49,12 @@ function(input, output) {
     
     state <- c(gene = input$copyNumber, mRNA = 0, protein = 0)
     
+    all_results_df <- data.frame()
+    
     # loop through each promoter strength value
-    for (name in names(k1_values())) {
+    for (name in names(k1_values)) {
       
-      k1_value <- k1_values()[[name]]  # access reactive k1 values
+      k1_value <- k1_values[[name]]  # access reactive k1 values
       
       # and assign promoter strength value to parameters vector
       parameters["k1"] <- k1_value
@@ -78,41 +77,46 @@ function(input, output) {
       # convert ODE output to a data frame
       out_df <- as.data.frame(out)
       
-      # Ensure time column exists
-      if (!"time" %in% colnames(out_df)) {
-        out_df$time <- times  # Add time column if missing
-      }
-      
       # add column and name to identify the k1 (promoter)
       out_df$k1 <- name
       
-      # add the results to our data frame
+      # add the results to the data frame
       all_results_df <- rbind(all_results_df, out_df)
     }
     
-    return(all_results_df)
-  })
-  
-  output$plot <- renderPlot({
-    # Get all results
-    results <- all_results()
-    
     # melt the data frame for ggplot2
-    results_melt <- melt(results, id.vars = c("time", "k1"))
+    results_melt <- melt(all_results_df, id.vars = c("time", "k1"))
     
     # filter out non-positive values, logarithms of zero or negative numbers are undefined
     results_melt <- results_melt[results_melt$value > 0, ]
     
-    ggplot(results_melt, aes(x = time, y = value, color = variable, linetype = k1)) +
-      geom_line() +
-      scale_y_log10() +
-      labs(title = "Gene Expression by Promoter Strength",
-           x = "Time (min)",
-           y = "Concentration (log scale)",
-           color = "Component",
-           linetype = "Promoter") +
-      theme_minimal()
+    output$plot <- renderPlot({
+      ggplot(results_melt, aes(x = time, y = value, color = variable, linetype = k1)) +
+        geom_line() +
+        scale_y_log10() +
+        labs(title = "Gene Expression by Promoter Strength",
+             x = "Time (min)",
+             y = "Concentration (log scale)",
+             color = "Component",
+             linetype = "Promoter") +
+        theme_minimal()
+      
+    }, height = 500)
     
-  }, height = 700)
+    # pull only protein and max time-point data for chart
+    bar_chart_data <- results_melt[results_melt$variable == "protein" & results_melt$time == max(results_melt$time), ]
+    
+    output$chart <- renderPlot({
+      ggplot(bar_chart_data, aes(x = k1, y = value, fill = k1)) +
+        geom_bar(stat = "identity") +
+        labs(title = "Total Protein Concentration by Promoter",
+             x = "Promoter",
+             y = "Total Protein Concentration") +
+        theme_minimal() +
+        theme(legend.position = "none")
+      
+    }, height = 500)
+    
+    }, ignoreNULL = FALSE)
   
 }
--- ui.R
+++ ui.R
@@ -6,53 +6,71 @@ dataset <- diamonds
 
 fluidPage(
   
-  titlePanel("DBTL Bootcamp: Modeling Constiuitive Promoter Expression"),
-  
-  layout_columns(
-    card( 
-      card_header("Model"),
-      withMathJax(),
-      div(
-        p("Transcription"),
-        p("$$\\frac{d[mRNA]}{dt} = k_{1}{[Gene]} - d_{1}{[mRNA]}$$"),
-        p("Translation"),
-        p("$$\\frac{d[Protein]}{dt} = k_{2}{[mRNA]} - d_{2}{[Protein]}$$")
+  titlePanel("DBTL Bootcamp: Modeling Constitutive Promoter Protein Expression"),
+  p("The following are the ordinary differential equations (ODEs) we are 
+        using in the iGEM DBTL Bootcamp to model protein expression under 
+        constitutive promoters. For more information, please see the modeling section in the DBTL Bootcamp 
+        documentation."),
+  sidebarLayout(
+    sidebarPanel(
+      card( 
+        card_header(tags$h3("Parameters & States")),
+        p("these parameters and states are used across all modeled constructs"),
+        numericInput('copyNumber','plasmid copy number (Gene)',17,min = 0,max = NA,step = 1,width = NULL),
+        numericInput('mrnaDeg','mRNA degradation (d1)',0.23,min = 0,max = NA,step = 0.01,width = NULL),
+        numericInput('rbsStr','RBS strength (k2)',8.23,min = 0,max = NA,step = 0.01,width = NULL),
+        numericInput('protDeg','protein degradation (d2)',0.02,min = 0,max = NA,step = 0.01,width = NULL)
+      ),
+      card( 
+        card_header(tags$h3("Promoter Strength Data (k1)")),
+        p("enter the name and strength (str) data of your promoter"),
+        fluidRow(
+          column(8, textInput('pro1Name', 'Name/Label', value = 'J23100')),
+          column(4, numericInput('pro1Str','str',1.00,min = 0,max = NA,step = 0.01))
+        ),
+        fluidRow(
+          column(8, textInput('pro2Name', 'Name/Label', value = 'J23101')),
+          column(4, numericInput('pro2Str','str',0.70,min = 0,max = NA,step = 0.01))
+        ),
+        fluidRow(
+          column(8, textInput('pro3Name', 'Name/Label', value = 'J23106')),
+          column(4, numericInput('pro3Str','str',0.47,min = 0,max = NA,step = 0.01))
+        ),
+        fluidRow(
+          column(8, textInput('pro4Name', 'Name/Label', value = 'J23116')),
+          column(4, numericInput('pro4Str','str',0.16,min = 0,max = NA,step = 0.01))
+        ),
+        fluidRow(
+          column(8, textInput('pro5Name', 'Name/Label', value = 'J23117')),
+          column(4, numericInput('pro5Str','str',0.06,min = 0,max = NA,step = 0.01))
+        ),
+        actionButton(
+          inputId = "submitData",
+          label = "Submit")
       )
     ),
-    card( 
-      card_header("Promoter Strength Data (k1)"),
-      p("enter the strength data of your promoter"),
-      fluidRow(
-        column(8, textInput('pro1Name', 'Name/Label', value = 'J23100')),
-        column(4, numericInput('pro1Str','strength',1.00,min = 0,max = NA,step = 0.01))
+    mainPanel(
+      card( 
+        card_header(tags$h3("Model")),
+        fluidRow(
+          column(4, img(src = "transcription-unit-k-d.png", style = "width: 100%; padding: 0;")),
+          column(8,
+                 withMathJax(),
+                 div(
+                   p("Transcription"),
+                   p("$$\\frac{d[mRNA]}{dt} = k_{1}{[Gene]} - d_{1}{[mRNA]}$$"),
+                   p("Translation"),
+                   p("$$\\frac{d[Protein]}{dt} = k_{2}{[mRNA]} - d_{2}{[Protein]}$$")
+                 )
+          )
+        )
       ),
       fluidRow(
-        column(8, textInput('pro2Name', 'Name/Label', value = 'J23101')),
-        column(4, numericInput('pro2Str','strength',0.70,min = 0,max = NA,step = 0.01))
-      ),
-      fluidRow(
-        column(8, textInput('pro3Name', 'Name/Label', value = 'J23106')),
-        column(4, numericInput('pro3Str','strength',0.47,min = 0,max = NA,step = 0.01))
-      ),
-      fluidRow(
-        column(8, textInput('pro4Name', 'Name/Label', value = 'J23116')),
-        column(4, numericInput('pro4Str','strength',0.16,min = 0,max = NA,step = 0.01))
-      ),
-      fluidRow(
-        column(8, textInput('pro5Name', 'Name/Label', value = 'J23117')),
-        column(4, numericInput('pro5Str','strength',0.06,min = 0,max = NA,step = 0.01))
+        column(6, plotOutput('plot')),
+        column(6, plotOutput('chart'))
       )
-    ),
-    card( 
-      card_header("Other Model Parameters & States"),
-      p("these parameters and states will be used across all modeled constructs"),
-      numericInput('mrnaDeg','mRNA degradation (d1)',0.23,min = 0,max = NA,step = 0.01,width = NULL),
-      numericInput('rbsStr','RBS strength (k2)',8.23,min = 0,max = NA,step = 0.01,width = NULL),
-      numericInput('protDeg','protein degradation (d2)',0.02,min = 0,max = NA,step = 0.01,width = NULL),
-      numericInput('copyNumber','plasmid copy number (Gene)',17,min = 0,max = NA,step = 1,width = NULL)
+
+
     )
-  ),
-  mainPanel(
-    plotOutput('plot')
   )
 )
